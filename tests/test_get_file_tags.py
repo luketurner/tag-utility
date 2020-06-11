05a53852f24588493b57ef7bc395282b46c4dad7
Luke Turner <github@luketurner.org>
2020-06-10 17:03:36 -0700

    add tests for get_foo functions

--- tests/test_get_file_tags.py
+++ tests/test_get_file_tags.py
@@ -4,3 +4,11 @@ import pony.orm as orm
 import tag
 
 from .util import *
+
+def test_get_file_tags_should_return_filetags(conn, tmpfile, sample_file_tag):
+  filetags = tag.get_file_tags(conn, tmpfile)
+  assert filetags.first().tag.name == "testtag"
+
+def test_get_file_tags_should_return_empty_cursor_for_missing_file(conn, tmpfile, sample_file_tag):
+  filetags = tag.get_file_tags(conn, "foo.txt")
+  assert len(filetags) == 0
--- tests/test_get_tag.py
+++ tests/test_get_tag.py
@@ -4,3 +4,11 @@ import pony.orm as orm
 import tag
 
 from .util import *
+
+def test_get_tag_should_return_tag(conn, tmpfile, sample_file_tag):
+  t = tag.get_tag(conn, "testtag")
+  assert t.name == "testtag"
+
+def test_get_tag_should_return_None_for_missing_tags(conn, tmpfile, sample_file_tag):
+  t = tag.get_tag(conn, "wrongtag")
+  assert t is None
